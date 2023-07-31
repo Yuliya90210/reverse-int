b4d6f78f027045858d92b22869792fcc960141a3
Yuliya <yuliya1grishkevich@gmail.com>
2023-07-31 19:51:26 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let i = 0;
+    let result = '';
+    if (n < 0) {
+        n = -n;
+    }
+    n = String(n);
+    while (i < n.length) {
+        result = `${n[i]}${result}`;
+        i++;
+    }
+    return Number(result);
 }
+
+
